d719e3919ae5ba77896094ba8a375613726c79a7
reiddouglas <reiddwilliamson@gmail.com>
2024-08-17 17:49:54 -0700

    added simple movement to slime

--- scripts/entities/enemies/Enemy.gd
+++ scripts/entities/enemies/Enemy.gd
@@ -17,23 +17,26 @@ func _ready():
 func _on_animation_finished(anim_name: StringName):
 	print(name + " has finished animation \'" + anim_name + "\'")
 	if anim_name.contains("attack"):
-		attacking = false
 		#disable attack for a few seconds after an attack finishes
 		_attack_enabled(false)
 		attack_timer.start()
 
 func _on_attack_timer_timeout():
 	#Enable all CollisionShape2D nodes in AttackBox
-	_attack_enabled(true)
+	for child in attack_box.get_children():
+		if child.is_class("CollisionShape2D"):
+			child.disabled = false
 	
 func _on_attack_box_area_entered(_hitbox: Area2D):
 	print(name + " triggers an attack.")
-	_attack()
-
-func _attack():
-	attacking = true
+	_attack_enabled(true)
 
 func _attack_enabled(input: bool):
-	for child in attack_box.get_children():
-		if child.is_class("CollisionShape2D"):
-			child.disabled = !input
+	attacking = input
+	if(input):
+		animation_tree.set("parameters/attacking_state/transition_request","attacking")
+		for child in attack_box.get_children():
+			if child.is_class("CollisionShape2D"):
+				child.set_deferred("disabled",input)
+	else:
+		animation_tree.set("parameters/attacking_state/transition_request","not_attacking")
