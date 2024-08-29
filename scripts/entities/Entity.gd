909c273e179f4c020a87109e05217bb2eeb55859
Reid W <reiddwilliamson@gmail.com>
2024-08-28 18:53:55 -0700

    added knockback to entities

--- scripts/entities/Entity.gd
+++ scripts/entities/Entity.gd
@@ -22,6 +22,7 @@ var speed_mult: float = 1: set = set_speed_mult, get = get_speed_mult
 var max_health: int: set = set_max_health, get = get_max_health
 var health: int: set = set_health, get = get_health
 var attack_power: int: set = set_attack_power, get = get_attack_power
+var knock_power: int: set = set_knock_power, get = get_knock_power
 var invuln_time: float = 1.0: set = set_invuln_time, get = get_invuln_time
 var stun_time: float = 1.0: set = set_stun_time, get = get_stun_time
 
@@ -103,6 +104,12 @@ func set_attack_power(new_attack_power: int):
 func get_attack_power():
 	return attack_power
 
+func set_knock_power(new_knock_power: int):
+	knock_power = max(new_knock_power,0)
+
+func get_knock_power():
+	return knock_power
+
 func set_invuln_time(new_invuln_time: float):
 	invuln_time = new_invuln_time
 
@@ -182,34 +189,27 @@ func die():
 	queue_free()
 
 func _on_hurt_box_area_entered(hitbox: Area2D):
+	#Only hitboxes can interact with hurtboxes
 	if hitbox.name != "HitBox":
 		return
 	var entity: Entity = hitbox.get_parent()
 	if entity:
 		#Deal damage to entity if hit
-		var damage = entity.get_attack_power()
-		set_health(get_health()-damage)
-		emit_signal("health_changed")
-		print(entity.name + " did " + str(damage) + " damage to " + name)
-		print(name + " has " + str(health) + "/" + str(max_health) + " health remaining.")
-		
-		#Kill entity if health reaches zero
-		if get_health() <= 0:
-			emit_signal("death")
-			die()
-		else:
-			#Invuln frames if entity survives the attack
-			_invuln_enabled(true)
-			invuln_timer.start()
-			#Hitstun for entity after being hit
-			_hitstun_enabled(true)
-			hitstun_timer.start()
+		if take_damage(entity):
+			return
+		#Invuln frames if entity survives the attack
+		_invuln_enabled(true)
+		invuln_timer.start()
+		#Hitstun for entity after being hit
+		_hitstun_enabled(true)
+		hitstun_timer.start()
+		#Knock enemy back as well
+		take_knockback(entity)
 
 	else:
 		printerr( str(entity) + " is not valid")
 
 func _on_invuln_timer_timeout():
-	print("Invuln ended")
 	_invuln_enabled(false)
 
 func _invuln_enabled(input: bool):
@@ -223,7 +223,6 @@ func _invuln_enabled(input: bool):
 			child.set_deferred("disabled",input)
 
 func _on_hitstun_timer_timeout():
-	print("Stun ended")
 	_hitstun_enabled(false)
 
 func _hitstun_enabled(input: bool):
@@ -232,3 +231,51 @@ func _hitstun_enabled(input: bool):
 		animation_tree.set("parameters/hit_state/transition_request","hit")
 	else:
 		animation_tree.set("parameters/hit_state/transition_request","not_hit")
+
+"""
+Function: take_damage
+Description: adjusts health values after taking damage from an opposing entity and checks for fatal damage
+Input:
+	entity - the entity dealing damage
+Output:
+	Boolean - true if the damage taken would be fatal (reduce health to zero), false otherwise
+"""
+func take_damage(entity: Entity):
+		var damage = entity.get_attack_power()
+		set_health(get_health()-damage)
+		emit_signal("health_changed")
+		print(entity.name + " did " + str(damage) + " damage to " + name)
+		print(name + " has " + str(health) + "/" + str(max_health) + " health remaining.")
+		
+		#Kill entity if health reaches zero
+		if get_health() <= 0:
+			emit_signal("death")
+			die()
+			return true
+		else:
+			return false
+
+"""
+Function: take_knockback
+Description: applies a force away from an entity based on its knockback value
+			 special considerations are taken if on the floor when the knockback is applied
+Input:
+	entity - the entity being interacted with
+Output:
+	void
+"""
+func take_knockback(entity: Entity):
+	#default angle if hit while planted on the ground
+	const FLOOR_ANGLE = PI/16
+	
+	var knockback = entity.get_knock_power()
+	var entity_pos = entity.global_position
+	var trajectory: float = global_position.angle_to_point(entity_pos)
+	
+	if is_on_floor():
+		if entity_pos.x - global_position.x < 0:
+			trajectory = -FLOOR_ANGLE
+		else:
+			trajectory = -PI + FLOOR_ANGLE
+	print(str(name) + " took knockback at angle " + str(trajectory) + " from " + str(entity.name))
+	set_velocity(Vector2.from_angle(trajectory) * knockback)
--- scripts/entities/enemies/Slime.gd
+++ scripts/entities/enemies/Slime.gd
@@ -11,16 +11,17 @@ func _ready():
 	super._ready()
 	
 	#physics stats
-	set_max_horizontal_velocity(250)
+	set_max_horizontal_velocity(100)
 	set_max_vertical_velocity(500)
-	set_friction(1000)
+	set_friction(100)
 	set_air_resistance(500)
 	set_jump_acceleration(Constants.GRAVITY + 500 * 60)
-	
-	#entity stats
-	set_max_health(200)
-	set_speed(50)
+
+	#enemy stats
+	set_max_health(500)
+	set_speed(1000)
 	set_attack_power(15)
+	set_knock_power(1000)
 	set_invuln_time(2)
 	set_stun_time(1)
 	
@@ -32,15 +33,13 @@ func _ready():
 
 func _physics_process(delta):
 	
-	print(distance_travelled)
-	
-	if not attacking:
+	if not attacking and not stunned:
 		if (ray_cast_front.is_colliding() or not ray_cast_below.is_colliding()) or distance_travelled >= max_distance:
 			scale.x *= -1
 			direction.x *= -1
 			distance_travelled = 0
-		position += direction * speed * delta
-		distance_travelled += speed * delta
-	
+		apply_movement(direction, speed, delta) 
+		distance_travelled += abs(velocity.x) * delta
+	apply_horizontal_ground_friction(delta)
 	apply_gravity(delta)
 	move_and_slide()
